c667587f4a5c682e03288d6a10c5f39cfde131bd
milanca89 <117099167+milanca89@users.noreply.github.com>
2023-11-04 18:25:49 +0530

    Add files via upload

---
+++ GridViewWithRowDragAndDrop.UI/GridViewWithRowDragAndDrop.UI/GridViewWithRowDragAndDrop.UI/Default.aspx
@@ -0,0 +1,108 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="GridViewWithFilter.UI._Default" %>
+
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="ajaxToolkit" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link id="Link1" href="../Assets/default.css" rel="stylesheet" type="text/css" media="screen"
+        runat="server" />
+    <script src="Scripts/jquery-1.7.1.min.js" type="text/javascript"></script>
+    <script src="Scripts/jquery.tablednd.js" type="text/javascript"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <ajaxToolkit:ToolkitScriptManager ID="scriptManager" runat="server">
+    </ajaxToolkit:ToolkitScriptManager>
+        <script type="text/javascript">
+
+            $(document).ready(function () {
+                $("#<%= grdViewOutstanding.ClientID %>").tableDnD({
+                    onDragClass: "DragTableRow",
+                    onDrop: function (table, row) {
+
+                        var rows = table.tBodies[0].rows, newOrder = '', dragged = '';
+                        dragged = $('#<%= eleDragged.ClientID %>').val();
+                        dragged += row.cells[10].innerText == undefined ? row.cells[10].textContent : row.cells[10].innerText + ":";
+                        for (var i = 1; i < rows.length; i++) {
+                            newOrder += rows[i].cells[10].innerText == undefined ? rows[i].cells[10].textContent : rows[i].cells[10].innerText + ":";
+                        }
+                        $('#<%= eleNewSeq.ClientID %>').val(newOrder);
+                        $('#<%= eleDragged.ClientID %>').val(dragged);
+                    }
+                });
+            });
+            </script>
+
+    <div>
+        <h1>
+            GridView With Row Drag And Drop</h1>
+    </div>
+    <table style="width:100%">
+        <tr>
+            <td>
+                &nbsp;
+            </td>
+            <td>
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <asp:UpdatePanel ID="upnlOutstanding" runat="server" ChildrenAsTriggers="true" UpdateMode="Conditional">
+                    <ContentTemplate>
+                        <h4>
+                            Outstanding Orders</h4>
+                        <br />
+
+                        <asp:GridView ID="grdViewOutstanding" runat="server" AutoGenerateColumns="False"
+                            BackColor="White" BorderColor="#999999" BorderStyle="Solid" CellPadding="5" ForeColor="Black"
+                            GridLines="Both" AllowPaging="True" CellSpacing="1" EmptyDataText="No outstanding orders found"
+                            CssClass="Grid" PageSize="10" AllowSorting="true" OnPageIndexChanging="grdViewOutstanding_PageIndexChanging"
+                             >
+                            <FooterStyle BackColor="#CCCCCC" />
+                            <Columns>
+                                <asp:BoundField DataField="SequenceId" HeaderText="SequenceId"  />
+                                <asp:BoundField DataField="Item" HeaderText="Item" />
+                                <asp:BoundField DataField="Order" HeaderText="Order" />
+                                <asp:BoundField DataField="Line" HeaderText="Line" />
+                                <asp:BoundField DataField="Status" HeaderText="Status" />
+                                <asp:BoundField DataField="ToLocation" HeaderText="ToLocation" />
+                                <asp:BoundField DataField="Qty" HeaderText="Qty" />
+                                <asp:BoundField DataField="RegDate" HeaderText="RegDate" DataFormatString="{0:dd/MM/yyyy}" />
+                                <asp:BoundField DataField="Location" HeaderText="Location" />
+                                <asp:BoundField DataField="AllocQty" HeaderText="AllocQty" />
+                                <asp:BoundField DataField="RecordId" HeaderText="RecordId" />
+                            </Columns>
+                            <PagerStyle HorizontalAlign="Center" CssClass="pgr" />
+                            <SelectedRowStyle BackColor="#000099" Font-Bold="True" ForeColor="White" />
+                            </asp:GridView>
+                    </ContentTemplate>
+                </asp:UpdatePanel>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                &nbsp;
+                <asp:Button ID="btnUpdateOrderSequence" runat="server" OnClick="btnUpdateOrderSequence_Click" Text="Update Order Sequence" />
+            </td>
+            <td>
+                &nbsp;
+            </td>
+        </tr>
+        <tr>
+            <td>
+                &nbsp;
+            </td>
+            <td>
+                &nbsp;
+            </td>
+        </tr>
+    </table>
+
+   <label for="eleNewSeq">New Sequence </label> <input type="text" value="" runat="server" id="eleNewSeq" style="width:100%"/>
+   <label for="eleDragged">Record Dragged </label>  <input type="text" value="" runat="server" id="eleDragged" style="width:100%" />
+
+    </form>
+</body>
+</html>
---
+++ GridViewWithRowDragAndDrop.UI/GridViewWithRowDragAndDrop.UI/GridViewWithRowDragAndDrop.UI/Default.aspx.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Linq.Expressions;
+
+namespace GridViewWithFilter.UI
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+
+        //Some calss to hold data for gridview.
+        [Serializable]
+        public class Outstanding
+        {
+            public int SequenceId { get; set; }
+            public int RecordId { get; set; }
+
+            public string Item { get; set; }
+            public string Order { get; set; }
+            public int Line { get; set; }
+            public int Status { get; set; }
+            public string ToLocation { get; set; }
+            public decimal Qty { get; set; }
+            public DateTime RegDate { get; set; }
+            public string Location { get; set; }
+            public decimal AllocQty { get; set; }
+
+
+            public List<Outstanding> GetOutstanding()
+            {
+                List<Outstanding> lstOrders = new List<Outstanding>();
+
+                lstOrders.Add(new Outstanding() { SequenceId=1, RecordId = 5000, Item = "CocaCola", Order = "000101", Line = 1, Status = 20, ToLocation = "Sydney", Qty = 2000, RegDate = new DateTime(2014, 1, 1), Location = "USA", AllocQty = 100 });
+                lstOrders.Add(new Outstanding() { SequenceId = 3, RecordId = 5001,Item = "BubbleGum", Order = "000101", Line = 1, Status = 20, ToLocation = "Sydney", Qty = 2500, RegDate = new DateTime(2014, 1, 11), Location = "USA", AllocQty = 300 });
+                lstOrders.Add(new Outstanding() { SequenceId = 4, RecordId = 5002, Item = "Coffee", Order = "000111", Line = 1, Status = 50, ToLocation = "Melbourne", Qty = 2500, RegDate = new DateTime(2014, 1, 10), Location = "USA", AllocQty = 100 });
+                lstOrders.Add(new Outstanding() { SequenceId = 5, RecordId = 5003, Item = "Sugar", Order = "000112", Line = 1, Status = 50, ToLocation = "Melbourne", Qty = 2300, RegDate = new DateTime(2014, 1, 10), Location = "NZ", AllocQty = 300 });
+                lstOrders.Add(new Outstanding() { SequenceId = 8, RecordId = 5004, Item = "Milk", Order = "000112", Line = 1, Status = 50, ToLocation = "Melbourne", Qty = 2300, RegDate = new DateTime(2014, 1, 10), Location = "NZ", AllocQty = 200 });
+                lstOrders.Add(new Outstanding() { SequenceId = 9, RecordId = 5005, Item = "Green Tea", Order = "000112", Line = 1, Status = 20, ToLocation = "Melbourne", Qty = 300, RegDate = new DateTime(2014, 1, 10), Location = "NZ", AllocQty = 220 });
+                lstOrders.Add(new Outstanding() { SequenceId = 10, RecordId = 5006, Item = "Biscuit", Order = "000131", Line = 1, Status = 70, ToLocation = "Perth", Qty = 200, RegDate = new DateTime(2014, 1, 12), Location = "IND", AllocQty = 10 });
+                lstOrders.Add(new Outstanding() { SequenceId = 11, RecordId = 5007, Item = "Wrap", Order = "000131", Line = 1, Status = 20, ToLocation = "Perth", Qty = 2100, RegDate = new DateTime(2014, 1, 12), Location = "IND", AllocQty = 110 });
+                return lstOrders;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                Outstanding objOutstanding = new Outstanding();
+                List<Outstanding> lstOutstandingOrders = new List<Outstanding>();
+                lstOutstandingOrders = objOutstanding.GetOutstanding();
+                grdViewOutstanding.DataSource = lstOutstandingOrders;
+                grdViewOutstanding.DataBind();
+
+                ViewState["lstOutstandingOrders"] = lstOutstandingOrders;
+                upnlOutstanding.Update();
+            }
+        }
+
+       
+        protected void grdViewOutstanding_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            if (ViewState["lstOutstandingOrders"] != null)
+            {
+                List<Outstanding> lstOutstandingOrders = (List<Outstanding>)ViewState["lstOutstandingOrders"];
+                grdViewOutstanding.PageIndex = e.NewPageIndex;
+                ViewState["lstOutstandingOrders"] = lstOutstandingOrders;
+                grdViewOutstanding.DataSource = lstOutstandingOrders;
+                grdViewOutstanding.DataBind();
+               
+                upnlOutstanding.Update();
+
+            }
+        }
+
+        protected void btnUpdateOrderSequence_Click(object sender, EventArgs e)
+        {
+            int recordId = 0;
+            int seq = 0;
+            Dictionary<int, int> recordIdSeq = new Dictionary<int, int>();
+            List<int> recordIds = new List<int>();
+            List<int> sequences = new List<int>();
+         
+            foreach (GridViewRow row in grdViewOutstanding.Rows)
+            {
+                seq = int.Parse(row.Cells[0].Text.Trim());
+                sequences.Add(seq); // List of Sequence Id
+            }
+
+            if (!string.IsNullOrEmpty(eleNewSeq.Value)) // New Sequenced RecordIds
+            {
+                string[] allNewOrderdRecordId = eleNewSeq.Value.Trim().Split(':'); // Get New Order of Record Ids
+                for (int i = 0; i < allNewOrderdRecordId.Length; i++)
+                {
+                    if (int.TryParse(allNewOrderdRecordId[i], out recordId))
+                    {
+                        recordIds.Add(recordId);
+                    }
+                }
+            }
+            if (recordIds.Count > 0)
+            {
+                sequences.Sort();
+                for (int i = 0; i < recordIds.Count; i++)
+                {
+                    recordIdSeq.Add(recordIds[i], sequences[i]); //Setup new order -> Which sequenceId is assinged to which Record
+                }
+            }
+            //For the purpose of this demo this manipulation is done on the viewstate data. Ideally you may want to save the new sequencing in the database and rebind gridview
+             if (ViewState["lstOutstandingOrders"] != null)
+             {
+                List<Outstanding> lstOutstandingOrders = (List<Outstanding>)ViewState["lstOutstandingOrders"];
+                List<Outstanding> lstOutStandingResequenced = new List<Outstanding>();
+                int i = 0;
+                foreach (Outstanding objTemp in lstOutstandingOrders)
+                {
+                    objTemp.SequenceId = recordIdSeq[objTemp.RecordId];  // Assign New Sequence Id 
+                    i++;
+                    lstOutStandingResequenced.Add(objTemp);
+                }
+                if (lstOutStandingResequenced.Count > 0)
+                {
+                    lstOutStandingResequenced = lstOutStandingResequenced.OrderBy(x => x.SequenceId).ToList();
+                    ViewState["lstOutstandingOrders"] = lstOutStandingResequenced;
+                    grdViewOutstanding.DataSource = lstOutStandingResequenced;
+                    grdViewOutstanding.DataBind();
+                    upnlOutstanding.Update();
+                }
+
+            }
+
+
+          
+
+        }
+        
+    }
+
+
+}
+
+
